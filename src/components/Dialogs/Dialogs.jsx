a2ac4ad56b15b7d096f61356335fcbbb55099143
unknown <valyameln@gmail.com>
2022-07-09 11:22:32 +0300

    використала метод масиву map для відображення елементів масиву

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,43 +3,62 @@ import s from './Dialogs.module.css';
 import { NavLink } from "react-router-dom";
 
 const DialogItem = (props) => {
-  
+
   let path = '/dialogs/' + props.id;
-  
   return (
     <li className={s.item}>
-      <NavLink to={path}>{props.name}</NavLink>
+      <NavLink className={({ isActive }) => (isActive ? s.active : s.item)} to={path}>
+        {props.name}
+      </NavLink>
     </li>
   )
 }
 
 const Message = (props) => {
-  
+
   return (
     <li className={s.message}>{props.message}</li>
   )
 }
 
+
+let dialogsData = [
+  { id: 1, name: "Robert" },
+  { id: 2, name: "Patricia" },
+  { id: 3, name: "Elizabetht" },
+  { id: 4, name: "Thomas" },
+  { id: 5, name: "Anthony" },
+  { id: 6, name: "Kevin" },
+  { id: 7, name: "Amanda" },
+];
+
+let dialogsElement = dialogsData.map(
+  (dialogElement) => <DialogItem name={dialogElement.name} id={dialogElement.id} />
+);
+let messageData = [
+  { id: 1, message: "Ті, що сплять, не роблять помилок." },
+  { id: 2, message: "Дія не завжди приведе до щастя, але до щастя привести може лише дія." },
+  { id: 3, message: "Вершина ідеальності – в простоті." },
+  { id: 4, message: "А життя – як лампочка: може перегоріти в будь-який момент." },
+  { id: 5, message: "Є багато речей, на які розумна людина не хотіла б звертати уваги." },
+];
+let messageElement = messageData.map(
+  (messageEl => <Message message={messageEl.message} />)
+)
+
 const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <ul className={s.dialogs_items}>
-
-        <DialogItem name="Robert" id="1" />
-        <DialogItem name="Patricia" id="2" />
-        <DialogItem name="Elizabetht" id="3" />
-        <DialogItem name="Thomas" id="4" />
-        <DialogItem name="Anthony" id="5" />
-        <DialogItem name="Kevin" id="6" />
-        <DialogItem name="Amanda" id="7" />
-       
+        {
+          dialogsElement
+        }
       </ul>
+
       <ul className={s.dialogs_messages}>
-        <Message message='Ті, що сплять, не роблять помилок.'/>
-        <Message message='Дія не завжди приведе до щастя, але до щастя привести може лише дія.'/>
-        <Message message='Вершина ідеальності – в простоті'/>
-        <Message message='А життя – як лампочка: може перегоріти в будь-який момент.'/>
-        <Message message='Є багато речей, на які розумна людина не хотіла б звертати уваги.'/>
+        {
+          messageElement
+        }
       </ul>
     </div>
   )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,19 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 
+let postsData = [
+  { id: 1, message: "post 1", likesCount: 15 },
+  { id: 2, message: "post 2", likesCount: 25 },
+  { id: 3, message: "post 3", likesCount: 8 },
+  { id: 4, message: "post 4", likesCount: 10 },
+  { id: 5, message: "post 5", likesCount: 17 },
+];
+
+let postsElements = postsData.map(
+  (postElement) =>
+    <Post message={postElement.message} likesCount={postElement.likesCount} />
+);
+
 const MyPosts = () => {
   return (
 
@@ -14,11 +27,10 @@ const MyPosts = () => {
         <button className={s.button}>Remove Post</button>
       </div>
       <ul className={s.posts}>
-        <Post message="post 1" likesCount="15" />
-        <Post message="post 2" likesCount="20"/>
-        <Post message="post 3" likesCount="8"/>
-        <Post message="post 4" likesCount="22"/>
-        <Post message="post 5" likesCount="10"/>
+        {
+          postsElements
+        }
+
       </ul>
     </div>
 
