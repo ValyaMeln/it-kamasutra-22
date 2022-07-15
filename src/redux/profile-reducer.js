3c4d15b7d394502eb2a9976eecfb27311f29f81f
unknown <valyameln@gmail.com>
2022-07-15 13:56:13 +0300

    створила свій store redux  (у.42)

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,8 +1,23 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+// let sum = (a = -1, b = 0) => {
+//   return a + b;
+// }
+// console.log(sum(5, 2));
+let initialState = {
+  posts: [
+    { id: 1, message: 'post 1', likesCount: 15 },
+    { id: 2, message: 'post 2', likesCount: 25 },
+    { id: 3, message: 'post 3', likesCount: 8 },
+    { id: 4, message: 'post 4', likesCount: 10 },
+    { id: 5, message: 'post 5', likesCount: 17 },
+  ],
+  newPostText: 'it kamasutra.com'
+};
 
+const profileReducer = (state = initialState, action) => {
+  // debugger;
   switch (action.type) {
     case ADD_POST:
       let newPost = {
