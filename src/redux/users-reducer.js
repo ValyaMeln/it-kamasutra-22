4f7bcc589447fe01ff89c8e24f22923a3c901e16
unknown <valyameln@gmail.com>
2022-07-20 21:51:27 +0300

    відобразила усіх юзерів, по 5 чоловік на сторінці (у.55)

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,13 +4,85 @@ const UNFOLLOW = 'UN-FOLLOW';
 
 const SET_USERS = 'SET-USERS';
 
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT';
+
 // const SEND_NAME = 'SEND-NAME';
 
 let initialState = {
-  users:
-  
-  [
-    // {
+  users: [],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 2
+
+
+}
+
+
+const usersReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case FOLLOW: {
+      return {
+        ...state,
+        // users: [...state.users],
+        users: state.users.map(u => {
+          if (u.id === action.userId) {
+            return { ...u, followed: true }
+          }
+          return u;
+        })
+      };
+    }
+    case UNFOLLOW: {
+      return {
+        ...state,
+        // users: [...state.users],
+        users: state.users.map(u => {
+          if (u.id === action.userId) {
+            return { ...u, followed: false }
+          }
+          return u;
+        })
+      };
+    }
+    case SET_USERS: {
+      return {
+        ...state,
+        users: action.users
+      };
+    }
+    case SET_CURRENT_PAGE: {
+      return {
+        ...state,
+        currentPage: action.currentPage
+      };
+    }
+    case SET_TOTAL_USERS_COUNT: {
+      return {
+        ...state,
+        totalUsersCount: action.count
+      };
+    }
+    default:
+      return state;
+  }
+
+}
+
+//!          Action Creator(AC)
+
+export const followAC = (userId) => ({ type: FOLLOW, userId })
+export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
+export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage: currentPage })
+export const setUsersTotalCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
+
+
+
+export default usersReducer;
+
+ // {
     //   id: 1, photoUrl: 'https://images.pexels.com/photos/1236678/pexels-photo-1236678.jpeg?auto=compress&cs=tinysrgb&w=600',
     //   followed: true, fullName: 'Robert', status: 'I am a boss', location: { city: 'Kyiv', country: 'Ukraine' }
     // },
@@ -38,71 +110,3 @@ let initialState = {
   //     id: 7, photoUrl: 'https://images.pexels.com/photos/5992535/pexels-photo-5992535.jpeg?auto=compress&cs=tinysrgb&w=600',
   //     followed: false, name: 'Amanda', status: 'I am a boss 7', location: { city: 'Kyiv', country: 'Ukraine' }
   //   },
-  ],
-
-
-}
-
-
-const usersReducer = (state = initialState, action) => {
-
-  switch (action.type) {
-    case FOLLOW: {
-      return {
-        ...state,
-        // users: [...state.users],
-        users: state.users.map(u => {
-          if (u.id === action.userId) {
-            return { ...u, followed: true }
-          }
-          return u;
-        })
-
-      };
-
-    }
-    case UNFOLLOW: {
-
-      return {
-        ...state,
-        // users: [...state.users],
-        users: state.users.map(u => {
-          if (u.id === action.userId) {
-            return { ...u, followed: false }
-          }
-          return u;
-        })
-
-      };
-
-    }
-    case SET_USERS: {
-      return {
-        ...state,
-        users: [...state.users, ...action.users]
-      };
-
-    }
-
-    default:
-      return state;
-  }
-
-}
-
-//!          Action Creator(AC)
-
-export const followAC = (userId) => ({ type: FOLLOW, userId })
-export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
-export const setUsersAC = (users) => ({ type: SET_USERS, users })
-// export const updateNewMessageBodyCreator = (body) => {
-//   return {
-//     type: UPDATE_NEW_MESSAGE_BODY,
-//     body: body
-//   }
-// }
-
-
-
-
-export default usersReducer;
