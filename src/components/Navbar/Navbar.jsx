0d8717bf4b745f5ce84163e7515056eee00eff31
unknown <valyameln@gmail.com>
2022-07-20 16:26:51 +0300

    створила класову компоненту (у.53)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,6 +18,9 @@ const Navbar = () => {
         <NavLink to='/news' className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
       </div>
       <div className={s.item} >
+        <NavLink to='/users' className={navData => navData.isActive ? s.active : s.item}>Users</NavLink>
+      </div>
+      <div className={s.item} >
         <NavLink to='/music' className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
       </div>
       <div className={s.item} >
--- src/components/Users/Users_byla_funkcionalna_komponenta.jsx
+++ src/components/Users/Users_byla_funkcionalna_komponenta.jsx
@@ -3,34 +3,35 @@ import React from "react";
 import s from "./Users.module.css"
 import userPhoto from "../../assets/images/user_logo.png"
 
+let Users = (props) => {
+ 
+  // debugger;
+  let getUsers = () => {
+   
+    if (props.users.length === 0) {
+      
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+          props.setUsers(response.data.items)
+        });
 
-
-class Users extends React.Component {
-
-  constructor(props) {
-    super(props);
-    // alert('Hyyyy');
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(response => {
-        this.props.setUsers(response.data.items)
-      });
-
+    }
   }
-
-
-
-  render() {
-    return <div>
+  
+  // debugger;
+  return (
+    <div>
+      <button onClick={getUsers}>Get Users</button>
       {
-        this.props.users.map(u => <div key={u.id} className={s.wrapper} >
+        props.users.map(u => <div key={u.id} className={s.wrapper} >
           <div className={s.wrapper_avatar}>
             <div>
               <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.user_photo} alt="user photo" />
             </div>
             <div>
               {u.followed
-                ? <button onClick={() => { this.props.unfollow(u.id) }} className={s.follow}>UnFollow</button>
-                : <button onClick={() => { this.props.follow(u.id) }} className={s.follow}>Follow</button> //передаємо через пропси діспач функцію follow
+                ? <button onClick={() => { props.unfollow(u.id) }} className={s.follow}>UnFollow</button>
+                : <button onClick={() => { props.follow(u.id) }} className={s.follow}>Follow</button> //передаємо через пропси діспач функцію follow
               }
             </div>
           </div>
@@ -46,11 +47,6 @@ class Users extends React.Component {
           </div>
         </div>)
       }
-    </div>
-  }
-
+    </div>)
 }
-
-
-
 export default Users;
