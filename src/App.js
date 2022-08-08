bbba27f357e54e8ad28545f1ded03ec1b3a8fa40
unknown <valyameln@gmail.com>
2022-08-08 19:56:45 +0300

    Налаштування додатка React для розгортання на сторінках GitHub (у.95)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';  // React з папки node_modules
+import React, { Component, Suspense } from 'react';  // React з папки node_modules
 import './App.css';
 import Footer from './components/Footer/Footer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import News from './components/News/News';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from "react-router-dom";
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
+// import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import Login from './components/Login/Login';
 import { connect } from "react-redux";
@@ -14,7 +14,9 @@ import { initializeApp } from "./redux/app-reducer";
 import { compose } from 'redux';
 import Preloader from './components/common/Preloader/Preloader';
 
-
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+// const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 
 class App extends Component {
@@ -23,7 +25,7 @@ class App extends Component {
     this.props.initializeApp();
   }
   render() {
-    
+
     if (!this.props.initialized) {
       return <Preloader />
     }
@@ -39,30 +41,32 @@ class App extends Component {
 
 
         <div className='app-wrapper-content'>
-          <Routes>
-            {/* //Route слідкують за урлом url */}
-            <Route path='/dialogs/' element={<DialogsContainer
-            // store={props.store} 
-            />} />
+          <Suspense fallback={<div>Завантаження...</div>}>
+            <Routes>
+              {/* <Suspense fallback={<Preloader />}> */}
+              {/* //Route слідкують за урлом url */}
+              <Route path='/dialogs/' element={<DialogsContainer
+              // store={props.store} 
+              />} />
 
-            {/* <Route path='/profile/:userId' element={<ProfileContainer
+              {/* <Route path='/profile/:userId' element={<ProfileContainer
           // store={props.store}
           />}
           /> */}
-            <Route path="/profile/" element={<ProfileContainer />}>
-              <Route path=":userId" element={<ProfileContainer />} />
-            </Route>
-            {/* <Route path='/profile/:userId' element={<ProfileContainer />} />
+              <Route path="/profile/" element={<ProfileContainer />}>
+                <Route path=":userId" element={<ProfileContainer />} />
+              </Route>
+              {/* <Route path='/profile/:userId' element={<ProfileContainer />} />
           <Route path='/profile' element={<ProfileContainer />} /> */}
 
-            <Route path='/news/' element={<News />} />
+              <Route path='/news/' element={<News />} />
 
-            <Route path='/users/' element={<UsersContainer />} />
+              <Route path='/users/' element={<UsersContainer />} />
 
-            <Route path='/login/' element={<Login />} />
-
-          </Routes>
-
+              <Route path='/login/' element={<Login />} />
+              {/* </Suspense> */}
+            </Routes>
+          </Suspense>
         </div>
         <Footer />
 
